99e5c6a4e8edbe1d533b34abc3b83e74e2991c20
Arpit0825 <80036699+Arpit0825@users.noreply.github.com>
2021-07-23 15:03:01 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzn2ZDgEIuxV48cPN2l5ZgjHmPFauuMbM",
+    authDomain: "wily-bb108.firebaseapp.com",
+    projectId: "wily-bb108",
+    storageBucket: "wily-bb108.appspot.com",
+    messagingSenderId: "294518211243",
+    appId: "1:294518211243:web:1d6e892de6f474f6b8edee"
+  };
+
+ //Initializing Firebase
+ firebase.initializeApp(firebaseConfig);
+ export default firebase.firestore(); 
